69cd381feff29fdfa3f454b77c59345d6333b6a2
MDai08 <64835353+MDai08@users.noreply.github.com>
2022-04-12 08:11:39 +0800

    Add files via upload

---
+++ Example1/n=2000/alpha = 1.25/Gdatag.m
@@ -0,0 +1,51 @@
+%% Generate sample paths for SDE with Brownian motion
+% dX_t = g(X_t)dt +sigma*dB_t
+% dX_t = f(X_t)dt +sigma*dB_t
+% g(X_t) = 4*x-4*x.^3-beta*(x.^2+2*x+1)
+% f(X_t) = 4*x-4*x.^3
+% beta = 2;
+tic;
+clear;
+randn('state',100);
+rand('state',100);
+format long;
+
+tic;
+alpha = 1.25;
+beta = 0;
+gam = 1;
+delta = 0;
+
+T = 200; 
+b = 1;
+[n,N] = size(beta);
+sigma = @(x)                    1; 
+f = @(b,x)                    x-x.^3; 
+h1 = @(x)                       1;
+% f = @(beta,x)           beta*(x.^2+2*x+1); 
+% f = @(x)    x-x^3;
+M = 2000000;                                                                   %the number of sample path
+dt=T/M;                                                                     %the time step
+t=0:dt:T;
+
+X = zeros(M+1,N);
+X(1,:) = -1;                                                                   %initial point  
+for j = 1 : N
+    for i = 1 : M
+        Z1 = sqrt(dt)*randn;
+        Z2 = stblrnd(alpha,beta,gam,delta);
+        X(i+1,j) = X(i,j) + f(beta(j),X(i,j))*dt + sigma(X(i,j))*Z1+ h1(X(i))*(dt)^(1/alpha)*Z2; %alpha !=1
+    end
+end
+
+N = 2000;
+st = M/N;
+for i = 1 : N+1
+    Y(i) = X(1+(i-1)*st);
+end
+s = 0 : T/N : T;
+plot(s,Y,'r*')
+
+toc
+save  data  Y
+
---
+++ Example1/n=4000/alpha = 1.25/test.m
@@ -0,0 +1,125 @@
+%% Explicit form of the estimator
+%  kernel matrix G, block-diagonal weight matrix A
+%  Q=-inv(I+CGA)*C*F (no Levy)
+%  Q=-inv(I+CGA)*C*(F+Z) (with Levy) 
+%  F=1/2*sum(tr[D(z)K(z,z')'']|z'=z_j)
+                                                   
+%%  initial data
+clear,clc;
+
+V = load('data.mat');           %% samples with levy
+
+y = V.Y';                    
+[m,n] = size(y);
+C = 1;                          %% parameter of weight                           
+alpha = 1.25;                   %% parameter of Levy process
+l = 1.4;                 %% superparameter of kernel
+%r = @(y)    4*y-4*y.^3;         %% drift term of known
+d = @(y)     1;                 %% diffusion term
+A = eye(m);                     %% block-diagonal weight matrix
+I = eye(m);
+
+%% calculate vector Q
+G = zeros(m);                   %% kernel matrix
+for i = 1 : m
+    for j = 1 : m
+        G(i,j) = exp(-(y(i)-y(j))^2/(2*l^2))/(l^2)-(exp(-(y(i)-y(j))^2/(2*l^2))*((y(i)-y(j))^2))/(l^4);
+    end
+end
+
+F = zeros(m,1);                 %% 求梯度F(公式中的梯度y)
+for i = 1 : m
+    for j = 1 : m
+        F(i) = F(i) + exp(-(y(i)-y(j))^2/(2*l^2))*((y(i)-y(j))/(l^4))+exp(-(y(i)-y(j))^2/(2*l^2))*(2*(y(i)-y(j))/(l^4))-exp(-(y(i)-y(j))^2/(2*l^2))*((y(i)-y(j))^3/(l^6));
+    end
+end
+
+Z = zeros(m,1);
+cons = alpha*gamma((1+alpha)/2)/(2^(1-alpha)*sqrt(pi)*gamma(1-alpha/2));  %the constant of alpha-stable symmetric jump measure
+% f = @(y) cons*(abs(y).^(-1-alpha)).*(exp(-(y(i)-(y(j)+y)).^2/(2*l.^2))-exp(-(X(i)-X(j)).^2/(2*l.^2)));
+for i=1:m
+    for j=1:m       
+        g = @(z) cons*(abs(z).^(-1-alpha)).*(-((y(i)-(y(j)+z))/(l.^2)).*exp(-(y(i)-(y(j)+z)).^2/(2*l.^2))+((y(i)-y(j))/(l.^2)).*exp(-(y(i)-y(j)).^2/(2*l.^2)));     
+        Z(i) = Z(i)+integral(g,-Inf,-0.001)+integral(g,0.001,Inf);
+    end
+end
+
+% R = zeros(m,1);
+% for i = 1 : m
+%     R(i) = r(y(i));
+% end
+
+% Q = -inv(I+C*G*A)*C*(G*R+F/2+Z);
+
+Q = -inv(I+C*G*A)*C*(F/2+Z);
+%% Calculate the function P
+step = 3/(m-1);
+x = -1.5:step:1.5;  
+
+L = zeros(m,1);         %求L(公式中的y)
+for i=1:m
+    for j=1:m
+        L(i) = L(i)+(exp(-(x(i) - y(j))^2/(2*l^2))*(x(i) - y(j))^2)/(l^4) - exp(-(x(i) - y(j))^2/(2*l^2))/l^2;       
+    end
+end
+
+J = zeros(m,1);         %求积分项
+for i=1:m
+    for j=1:m
+       f = @(z) cons*(abs(z).^(-1-alpha)).*(exp(-(x(i)-(y(j)+z)).^2/(2*l.^2))-exp(-(x(i)-y(j)).^2/(2*l.^2)));
+       J(i) = J(i)+integral(f,-Inf,-0.001)+integral(f,0.001,Inf);
+    end
+end
+
+K = zeros(m);           %梯度kernel
+for i=1:m
+    for j=1:m
+        K(i,j) = ((x(i)-y(j))/(l^2))*(exp(-(x(i)-y(j)).^2/(2*l.^2)));
+    end
+end
+
+P = zeros(m,1);   %求\psi
+for i=1:m
+%     P(i) = -C*(K(i,:)*A*Q+(K(i,:)*R+L(i)/2+J(i)));
+     P(i) = -C*(K(i,:)*A*Q+(L(i)/2+J(i)));
+end
+
+% PF = zeros(m-2,1);
+% RPF = zeros(m-2,1);
+% RRPF = zeros(m-2,1);
+% for i=1:m-2
+%     PF(i)=(P(i+2)-P(i))/(2*step);
+%     RPF(i)=(x(i+1)-x(i+1)^3); 
+%     RPF(i)=(-1)*(x(i+1)^2+2*x(i+1)+1); 
+% end
+
+% for i = 1 : m
+%     RRPF(i) = (-1)*(y(i)^2+2*y(i)+1); 
+% end
+
+PF = zeros(m-2,1);
+for i=1:m-2
+    PF(i)=(P(i+2)-P(i))/(2*step);
+    RPF(i)=x(i+1)-x(i+1)^3;
+end
+
+
+
+
+
+
+% d = (1./(2.*(m))).*(sum(RRPF.^2))
+% d1 = (1./(2.*(m))).*(sum(Q.^2))
+
+%% 误差值
+E(1)=0;
+for i=1:m-2
+    E(i+1)=E(i)+(PF(i)-RPF(i)).^2;
+end
+E = E(m-1)/(m-2);
+
+
+plot(x(2:end-1),PF,'r-',x(2:end-1),RPF,'b-')
+
+
+save drift140 m x y G F Q L K P PF RPF E
